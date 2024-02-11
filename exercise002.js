9664cdc3cdee9a334ef49445a87828a63c3f81f1
Chae Cramb <chaecramb@gmail.com>
2024-02-11 11:26:45 +0000

    feat: complete exercise 2 - task 2

--- exercise002.js
+++ exercise002.js
@@ -1,15 +1,31 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const starWarsApi = "https://swapi.dev/api/films/1/";
 
 const fetchData = async (apiEndPoint) => {
   try {
     const response = await fetch(apiEndPoint);
     const jsonData = await response.json();
     console.log(jsonData);
+    return jsonData;
   } catch (e) {
     console.log(error);
   }
 };
 
 fetchData(jsonTypicode);
+const starWarsEpisode2Data = await fetchData(starWarsApi);
+
+const planetsData = starWarsEpisode2Data.planets.map((planetUrl) =>
+  fetchData(planetUrl)
+);
+
+const logPlanetNames = async (planetsData) => {
+  let planets = await Promise.all(planetsData);
+  planets.forEach((planet) => {
+    console.log(planet.name);
+  });
+};
+
+logPlanetNames(planetsData);
